4eccfe06070ae4777d8fe317c114327e563b3850
Espen Gunnarsen <espen.gunnarsen@byr.oslo.kommune.no>
2020-09-16 07:55:23 +0200

    pytest

--- how_to_pytest/test_my_program.py
+++ how_to_pytest/test_my_program.py
@@ -1,11 +1,17 @@
 from my_program import double_it
 
 def test_double_it():
+    "Will execute, since the function name starts with 'test'."
     assert double_it(5) == 10
     assert double_it(6) == 14
+    assert double_it(0) == 52  # Never executed, this function is left immediately after the above fail. 
 
-def ztestz_useless_test():
-    assert False
+def test_double_it_description():
+    "Get descriptive output."
+    parameter = 10 
+    expected = (parameter * 2) + 0.05
+    result = double_it(parameter)
+    assert double_it(parameter) == expected, f"Failed because {result} != {expected}."
 
 
 
