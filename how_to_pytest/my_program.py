785a431e4bbb91caf59a744e07d13d6eeb7e36e8
Espen Gunnarsen <espen.gunnarsen@byr.oslo.kommune.no>
2020-09-22 08:04:13 +0200

    pytest

--- how_to_pytest/my_program.py
+++ how_to_pytest/my_program.py
@@ -1,3 +1,9 @@
 def double_it(x):
     return x * 2
 
+
+def bad_double_it(x):
+    return (x * 2) + 0.123
+
+
+
--- how_to_pytest/test_my_program.py
+++ how_to_pytest/test_my_program.py
@@ -1,17 +1,39 @@
-from my_program import double_it
+import pytest
+import time
+from my_program import double_it, bad_double_it
 
+
+@pytest.mark.main_test
 def test_double_it():
-    "Will execute, since the function name starts with 'test'."
-    assert double_it(5) == 10
-    assert double_it(6) == 14
-    assert double_it(0) == 52  # Never executed, this function is left immediately after the above fail. 
 
-def test_double_it_description():
-    "Get descriptive output."
+    """Will execute, since the function name starts with 'test'."""
+
+    assert double_it(5) == 10
+    assert double_it(6) == 14  # The problem where is of course the expectation, not the tested function.
+    assert double_it(0) == 52  # Never executed, this function is left immediately after the above fail.
+
+
+@pytest.mark.main_test
+def test_bad_double_it_description():
+    """Get descriptive output."""
+
     parameter = 10 
-    expected = (parameter * 2) + 0.05
-    result = double_it(parameter)
-    assert double_it(parameter) == expected, f"Failed because {result} != {expected}."
+    expected = 20
+    result = bad_double_it(parameter)
+    assert result == expected, f"Failed because {result} != {expected}."
+
+
+@pytest.mark.slow_test
+def test_pass():
+    time.sleep(1)
+    assert True
+
+
+@pytest.mark.skip  # Always skip it.
+def test_really_slow():
+    print("Oh, dear! I thought we agreed to skip this test?")
+    time.sleep(5)
+    assert False
 
 
 
