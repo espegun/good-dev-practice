3d9f64dbc5ee184c30e28c92bb5219b030a168dc
Espen Gunnarsen <espen.gunnarsen@byr.oslo.kommune.no>
2020-09-22 08:45:41 +0200

    Pytest finished for now.

--- how_to_pytest/my_program.py
+++ how_to_pytest/my_program.py
@@ -6,4 +6,9 @@ def bad_double_it(x):
     return (x * 2) + 0.123
 
 
+def calculate_rectangle_area(side1, side2):
+    return side1 * side2
+
+
+
 
--- how_to_pytest/test_my_program.py
+++ how_to_pytest/test_my_program.py
@@ -1,6 +1,6 @@
 import pytest
 import time
-from my_program import double_it, bad_double_it
+from my_program import double_it, bad_double_it, calculate_rectangle_area
 
 
 @pytest.mark.main_test
@@ -34,6 +34,29 @@ def test_really_slow():
     print("Oh, dear! I thought we agreed to skip this test?")
     time.sleep(5)
     assert False
+
+
+@pytest.fixture  # Function to run before tests to provide setup data
+def get_data():
+    input_data = {"side1": 2,
+                  "side2": 3}
+    return input_data
+
+
+@pytest.mark.test_using_fixture
+def test_calculate_rectangle_area(get_data):
+
+    # Note that the input data has the NAME of the fixture function. Could also be a list instead of dict.
+    assert calculate_rectangle_area(get_data["side1"], get_data["side2"]) == 6, "Area calculation failed!"
+
+
+# Run the same test function with different settings
+@pytest.mark.parametrize_test
+@pytest.mark.parametrize("input1, output", [(1, 2), (2, 4), (3, 6), (10, 20)])
+def test_double_it(input1, output):
+
+    assert double_it(input1) == output, "failed"
+
 
 
 
