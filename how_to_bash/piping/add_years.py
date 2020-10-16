da6267b46b7bd1d76bb726ae82f436c88d0a7c56
Espen Gunnarsen <espen.gunnarsen@byr.oslo.kommune.no>
2020-10-16 09:35:25 +0200

    Add python script.

---
+++ how_to_bash/piping/add_years.py
@@ -0,0 +1,11 @@
+import sys
+
+# print(sys.argv[0], sys.argv[1], sys.argv[2])
+org_years = int(sys.argv[1])
+add_years = int(sys.argv[2])
+#org_years = sys.argv[1]
+#add_years = sys.argv[2]
+new_years = org_years + add_years
+
+print(new_years)
+
